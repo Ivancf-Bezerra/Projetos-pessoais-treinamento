6be94b768042946d0637b33e873a48f5275bb212
Ivancf-Bezerra <122057613+Ivancf-Bezerra@users.noreply.github.com>
2023-02-22 16:03:57 -0300

    attjs

--- FormularioRPG/script.js
+++ FormularioRPG/script.js
@@ -4,3 +4,18 @@ function finiciar(){
         ficha.style.display = 'block';
     }
 }
+
+function fclasse(){
+
+    if(document.getElementById('guerreiro').checked) {
+        document.getElementById('armas-guerreiro').style.display = 'block';
+    }else {
+        document.getElementById('armas-guerreiro').style.display = 'none';   
+    }
+    if(document.getElementById('ladrao').checked) {
+        document.getElementById('armas-ladrao').style.display = 'block';
+    }else {
+        document.getElementById('armas-ladrao').style.display = 'none';   
+    }
+}
+    
