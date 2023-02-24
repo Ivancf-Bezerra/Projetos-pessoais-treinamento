06baed06cebc494be4a43afa4b3fbf84efad0b7a
Ivancf-Bezerra <122057613+Ivancf-Bezerra@users.noreply.github.com>
2023-02-24 09:46:21 -0300

    att

--- FormularioRPG/script.js
+++ FormularioRPG/script.js
@@ -17,5 +17,32 @@ function fclasse(){
     }else {
         document.getElementById('armas-ladrao').style.display = 'none';   
     }
+
+    if (!document.getElementById('guerreiro').checked) {
+        document.querySelectorAll('#armas-guerreiro input[type="checkbox"]').forEach(checkbox => {
+            checkbox.checked = false;
+        });
+    }
+    if (!document.getElementById('ladrao').checked) {
+        document.querySelectorAll('#armas-ladrao input[type="checkbox"]').forEach(checkbox => {
+            checkbox.checked = false;
+        });
+    }
+}
+
+function verificarQuantidadeDeArmas() {
+    var armasSelecionadas = 0;
+    var checkboxes = document.querySelectorAll(".armas-classes input[type='checkbox']");
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            armasSelecionadas++;
+        }
+    }
+    if (armasSelecionadas > 2) {
+        // Desmarcar a última checkbox selecionada
+        event.target.checked = false;
+        // Exibir um alerta informando que o limite de armas foi atingido
+        alert("Você só pode selecionar até 2 armas.");
+    }
 }
     
